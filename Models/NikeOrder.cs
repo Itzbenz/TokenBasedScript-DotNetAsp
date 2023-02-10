f93c719203aa74cc3892032836e21916b75dceab
itzbenz <49940811+Itzbenz@users.noreply.github.com>
2023-02-10 08:17:36 +0700

    w

--- Models/NikeOrder.cs
+++ Models/NikeOrder.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Mvc;
+
 namespace TokenBasedScript.Models;
 
 public class NikeOrder
@@ -38,7 +40,40 @@ public class NikeOrder
         public long? id { get; set; }
         public string orderEmail { get; set; }
         public string orderNumber { get; set; }
-        public VasBrtForm vasBrtForm { get; set; }
+        public string name { get; set; }
+        public string telephone { get; set; }
+        public string email { get; set; }
+        public string mobilePhoneNumber { get; set; }
+        public string address { get; set; }
+        public string postCode { get; set; }
+        public string town { get; set; }
+        public string district { get; set; }
+        
+  
+        public NikeOrder ToNikeOrder()
+        {
+            return new NikeOrder
+            {
+                id = id.ToString(),
+                orderEmail = orderEmail,
+                orderNumber = orderNumber,
+                vasBrtForm = new VasBrtForm
+                {
+                    name = name,
+                    telephone = telephone,
+                    email = email,
+                    mobilePhoneNumber = mobilePhoneNumber,
+                    optionsData = new VasBrtForm.OptionsData
+                    {
+                        name = name,
+                        address = address,
+                        postCode = postCode,
+                        town = town,
+                        district = district
+                    }
+                }
+            };
+        }
     }
 
     public class VasBrtForm
